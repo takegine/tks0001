b0daa96a7b5d84b021fe0ca2ecfe3f4f868fe58d
takegine <405773654@qq.com>
2020-05-22 20:07:32 +0800

    Delete link.bat

--- link.bat
+++
@@ -1,3 +0,0 @@
-mklink /J  "D:\Users\ace\Documents\GitHub\tks0001\scripts" "D:\PLAY\Steam\steamapps\common\dota 2 beta\game\dota_addons\tks0004\scripts\"
-
-mklink /J  "D:\Users\ace\Documents\GitHub\tks0001\panorama" "D:\PLAY\Steam\steamapps\common\dota 2 beta\content\dota_addons\tks0004\panorama\"
