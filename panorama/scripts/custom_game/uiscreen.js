c2fa6eb165aaba4b60571ba9904125d4c53b24fb
takegine <405773654@qq.com>
2020-05-29 09:42:14 +0800

    初始武将创建的补充1

--- panorama/scripts/custom_game/uiscreen.js
+++ panorama/scripts/custom_game/uiscreen.js
@@ -9,14 +9,14 @@ function OpenInfo( keys ){
 		sendnum=keys;
 	}
 	else {
-		if(keys==10){  sendnum={  	1:$('#FirstHero').GetChild(0).GetChild(0).itemname,
-									2:$('#FirstHero').GetChild(1).GetChild(0).itemname,
-									3:$('#FirstHero').GetChild(2).GetChild(0).itemname,
-									4:$('#FirstHero').GetChild(3).GetChild(0).itemname,
+		if(keys==10){  sendnum={  	1:$('#FirstHero').GetChild(0).GetChild(1).GetChild(0).id,
+									2:$('#FirstHero').GetChild(1).GetChild(1).GetChild(0).id,
+									3:$('#FirstHero').GetChild(2).GetChild(1).GetChild(0).id,
+									4:$('#FirstHero').GetChild(3).GetChild(1).GetChild(0).id,
 								};
 					}
-		else{ 	sendnum = $('#FirstHero').FindChild("hero"+keys).GetChild(0).itemname; }
-		//var itemname=$('#FirstHero').FindChild("hero"+keys).GetChild(0).itemname
+		else{ 	sendnum = $('#FirstHero').FindChild("hero"+keys).GetChild(1).GetChild(0).id; }
+		//var itemname=$('#FirstHero').FindChild("hero"+keys).GetChild(1).GetChild(0).id
 		//GameEvents.SendCustomGameEventToServer( "find_wujiang", {id: Players.GetLocalPlayer(),way: itemname,No:0} ); 
 				$("#FirstHero").visible = false;
 				$("#btn").visible = false; 
@@ -31,9 +31,11 @@ function wujiang_first(params) {
 	var itemName =params.event
 	var NewPanel = $.CreatePanel('Button', $('#FirstHero'),"hero"+num);
 		NewPanel.BLoadLayoutSnippet("QuestLine");
+        NewPanel.GetChild(0).SetUnit(itemName,'nil' ,true);
         //NewPanel.GetChild(0).scr = 'file://{resources}/images/custom_game/unithead/wujiang_'+itemName.slice(9)+'.jpg'
 		NewPanel.GetChild(1).text = $.Localize(itemName)
 		NewPanel.SetPanelEvent('onactivate',function() {OpenInfo(num) }  ) ;
+        $.CreatePanel('Panel', NewButton.GetChild(1),itemName);
 }
 
 /*function randomvote(num) {
