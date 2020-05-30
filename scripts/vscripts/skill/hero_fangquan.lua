3a006eb2568aac4efa0cdabad436e7fd20bf1b3a
takegine <405773654@qq.com>
2020-05-30 14:38:36 +0800

    新添技能：放权

--- scripts/vscripts/skill/hero_fangquan.lua
+++ scripts/vscripts/skill/hero_fangquan.lua
@@ -1,43 +1,51 @@
 --[[
-	Author: 西索酱
-	Date: 12.05.2020
-	小于50%的血增加护甲30+lvl*3
+    Author: 西索酱
+    Date: 30.05.2020
+    转移30%的属性给随机队友
 ]]
-skill_hero_fangquan = class ({})
 
-----------------------------------------------------------------------------------------
+function on_created(keys)
+    
+    self = keys.ability
+    local caster   = self:GetCaster()
+    local range	   = self:GetLevelSpecialValueFor("range",(self:GetLevel()-1))
+    local tFriend  = FindUnitsInRadius( caster:GetTeamNumber(), caster:GetOrigin(), nil, range, DOTA_UNIT_TARGET_TEAM_FRIENDLY,DOTA_UNIT_TARGET_HERO, 0, 0, false )
 
-function skill_hero_fangquan:OnSpellStart()
-	local caster   = self:GetCaster()
+    for k,v in pairs(tFriend)do 
+        if v==caster or v:GetName()==SET_FORCE_HERO then
+            table.remove(tFriend, k)
+        end
+    end
+    if #tFriend == 0 then return UF_FAIL_DEAD end
+
     local duration = self:GetLevelSpecialValueFor("duration",(self:GetLevel()-1))
-	local tFriend  = FindUnitsInRadius( caster:GetTeamNumber(), caster:GetOrigin(), nil, 999, DOTA_UNIT_TARGET_TEAM_FRIENDLY,DOTA_UNIT_TARGET_ALL, 0, 0, false )
-	local used  = false
-	local cdrun = 0 
+    local change = self:GetLevelSpecialValueFor("change",(self:GetLevel()-1))
+    self.Str     = math.ceil(caster:GetStrength()*change)
+    self.Agi     = math.ceil(caster:GetAgility()*change)
+    self.Int     = math.ceil(caster:GetIntellect()*change)
+    local sBuff  = "modifier_skill_hero_fangquan_buff_"
+    local sDebuff= "modifier_skill_hero_fangquan_debuff_"
 
-	if #tFriend == 0 then return UF_FAIL_DEAD
-	else
-		Timers(function()
-			if not used then
-				used = true
-				local target = tFriend[RandomInt(1, #tFriend)]
-				local change = caster:GetMaxHealth()*0.3
-				--caster 减change
-				--target 加change
-				
-			end
+    local target = tFriend[RandomInt(1, #tFriend)]        
 
-			if cdrun < duration then
-				cdrun= 1+cdrun
-				return 1
-			else
-				if target:IsAlive() then
-					--caster 加change
-					--target 减change
-				end
-			end
-		end)
-	end
+    for _,v in pairs({"Str","Agi","Int"}) do
+        self:ApplyDataDrivenModifier(caster, target, sBuff..v, {duration=duration})
+        target:SetModifierStackCount(sBuff..v, caster, self[v])
+
+        self:ApplyDataDrivenModifier(target, caster, sDebuff..v, nil)
+        caster:SetModifierStackCount(sDebuff..v, target,self[v])
+    end
+    
 end
 
+function on_destroy(keys)
+    local target = keys.target
+    local caster = keys.caster
+    local sDebuff= "modifier_skill_hero_fangquan_debuff_"
 
-----------------------------------------------------------------------------------------
+    for _,v in pairs({"Str","Agi","Int"}) do
+        if target:IsAlive() and caster:HasModifier(sDebuff..v) then
+            caster:RemoveModifierByNameAndCaster(sDebuff..v, target)
+        end
+    end
+end
