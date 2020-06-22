dc5acc89a9e502af3c083483098affa708d09a56
takegine <405773654@qq.com>
2020-06-22 22:09:22 +0800

    修复白玉环BUG

--- scripts/vscripts/skill/item_weapon_baiyuhuan_aura.lua
+++ scripts/vscripts/skill/item_weapon_baiyuhuan_aura.lua
@@ -1,17 +1,17 @@
---[[Author: Pizzalol
-	Date: 11.07.2015.
-	Deals damage based on the max HP of the target]]
+--[[Author: 西索酱
+	Date: 22.06.2020.
+	按目标最大生命值造成百分比伤害]]
 function HeartstopperAura( keys )
-	local caster = keys.caster
-	local ability = keys.ability
-	local target = keys.target
+	local caster  = keys.caster
+	local ability = keys.ability or caster:GetItemInSlot(2)
+	local target  = keys.target
+	local aura_damage   = keys.aura_damage
 	local target_max_hp = target:GetMaxHealth() / 100
-	local aura_damage = ability:GetLevelSpecialValueFor("aura_damage", (ability:GetLevel() - 1))
-	local aura_damage_interval = ability:GetLevelSpecialValueFor("aura_damage_interval", (ability:GetLevel() - 1))
 
+	if caster:GetUnitName() == SET_FORCE_HERO then return end
 	-- Shows the debuff on the target's modifier bar only if Necrophos is visible
 	local visibility_modifier = keys.visibility_modifier
-	if target:CanEntityBeSeenByMyTeam(caster) then
+	if  target:CanEntityBeSeenByMyTeam(caster) then
 		ability:ApplyDataDrivenModifier(caster, target, visibility_modifier, {})
 	else
 		target:RemoveModifierByName(visibility_modifier)
@@ -20,11 +20,11 @@ function HeartstopperAura( keys )
 	local damage_table = {}
 
 	damage_table.attacker = caster
-	damage_table.victim = target
-	damage_table.damage_type = DAMAGE_TYPE_PURE
-	damage_table.ability = ability
-	damage_table.damage = target_max_hp * -aura_damage * aura_damage_interval
-	damage_table.damage_flags = DOTA_DAMAGE_FLAG_HPLOSS -- Doesnt trigger abilities and items that get disabled by damage
+	damage_table.victim   = target
+	damage_table.ability  = ability
+	damage_table.damage   = target_max_hp * aura_damage 
+	damage_table.damage_type  = DAMAGE_TYPE_PURE
+	damage_table.damage_flags = DOTA_DAMAGE_FLAG_HPLOSS
 
 	ApplyDamage(damage_table)
 end
